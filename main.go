68dc017599f5f078f7a7dae0fc9ca5eb19e2706a
Igwe Chinonso <igwechinonso77@gmail.com>
2023-12-20 17:59:23 +0100

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	port := flag.String("port", "8080", "port to listen on")
 	flag.Parse()
+	fmt.Println("Hello, World!")
 	fmt.Printf("port: %s\n", *port)
 	fmt.Println(flag.Arg(0), "nono")
 	fmt.Println(flag.NArg(), "flag.NArg()")
